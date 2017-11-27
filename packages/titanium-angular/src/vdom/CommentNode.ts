898ef9f8b4a6221eb4c2dac6324a629d7d6a8c13
Jan Vennemann <jan.vennemann@gmx.net>
2017-11-28 00:09:50 +0100

    Set node name according to spec

--- packages/titanium-angular/src/vdom/CommentNode.ts
+++ packages/titanium-angular/src/vdom/CommentNode.ts
@@ -7,6 +7,7 @@ export class CommentNode extends AbstractTextualNode {
     constructor(text: string) {
         super(text);
 
+        this.nodeName = '#comment';
         this.nodeType = NodeType.Comment;
     }
 }
--- packages/titanium-angular/src/vdom/TextNode.ts
+++ packages/titanium-angular/src/vdom/TextNode.ts
@@ -7,6 +7,7 @@ export class TextNode extends AbstractTextualNode {
     constructor(text: string) {
         super(text);
 
+        this.nodeName = '#text';
         this.nodeType = NodeType.Text;
     }
 }
