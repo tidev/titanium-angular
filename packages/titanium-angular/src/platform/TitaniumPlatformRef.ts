7efbe0215d409d3eb4566502f96a5cb4903f6069
Jan Vennemann <jan.vennemann@gmx.net>
2017-12-02 02:29:24 +0100

    Add all platform independent UI views

--- packages/titanium-angular/src/platform/TitaniumPlatformRef.ts
+++ packages/titanium-angular/src/platform/TitaniumPlatformRef.ts
@@ -14,7 +14,7 @@ import {
     TitaniumElementRegistry
 } from '../vdom';
 
-type BootstrapperAction = () => Promise<NgModuleRef<any>>;
+type BootstrapFunction = () => Promise<NgModuleRef<any>>;
 export interface BootstrapOptions {
     /**
      * Optionally specify which `NgZone` should be used.
@@ -28,17 +28,17 @@ export interface BootstrapOptions {
 
 export class TitaniumPlatformRef extends PlatformRef {
 
-    private _bootstrapper: BootstrapperAction;
-    private platform: PlatformRef;
+    private _runBootstrap: BootstrapFunction;
+    private _platform: PlatformRef;
 
     constructor(platform: PlatformRef) {
         super();
 
-        this.platform = platform;
+        this._platform = platform;
     }
 
     bootstrapModuleFactory<M>(moduleFactory: NgModuleFactory<M>, options?: BootstrapOptions): Promise<NgModuleRef<M>> {
-        this._bootstrapper = () => this.platform.bootstrapModuleFactory(moduleFactory);
+        this._runBootstrap = () => this._platform.bootstrapModuleFactory(moduleFactory);
 
         console.log('bootstrapModuleFactory');
         this.bootstrapApp();
@@ -47,7 +47,7 @@ export class TitaniumPlatformRef extends PlatformRef {
     }
 
     bootstrapModule<M>(moduleType: Type<M>, compilerOptions: CompilerOptions | CompilerOptions[] = []): Promise<NgModuleRef<M>> {
-        this._bootstrapper = () => this.platform.bootstrapModule(moduleType, compilerOptions);
+        this._runBootstrap = () => this._platform.bootstrapModule(moduleType, compilerOptions);
 
         console.log('bootstrapModule');
         this.bootstrapApp();
@@ -58,7 +58,7 @@ export class TitaniumPlatformRef extends PlatformRef {
     bootstrapApp(): void {
         this.registerTitaniumViews();
 
-        this._bootstrapper().then(moduleRef => {
+        this._runBootstrap().then(moduleRef => {
             console.log('ANGULAR BOOTSTRAP DONE!');
         }, err => {
             console.log('ERROR BOOTSTRAPPING ANGULAR!');
@@ -68,24 +68,45 @@ export class TitaniumPlatformRef extends PlatformRef {
     }
 
     registerTitaniumViews() {
-        console.log('registerTitaniumViews');
         const titaniumElementRegistry: TitaniumElementRegistry = this.injector.get(ELEMENT_REGISTRY);
-        titaniumElementRegistry.registerElement('View', () => Ti.UI.createView, { typeName: 'Ti.UI.View' });
-        titaniumElementRegistry.registerElement('Window', () => Ti.UI.createWindow, { skipAddToDom: true, typeName: 'Ti.UI.Window'});
-        titaniumElementRegistry.registerElement('Label', () => Ti.UI.createLabel, { typeName: 'Ti.UI.Label'});
+        titaniumElementRegistry.registerElement('ActivityIndicator', () => Ti.UI.createActivityIndicator, { typeName: 'Ti.UI.ActivityIndicator' });
+        titaniumElementRegistry.registerElement('AlertDialog', () => Ti.UI.createAlertDialog, { typeName: 'Ti.UI.AlertDialog' });
+        titaniumElementRegistry.registerElement('Button', () => Ti.UI.createButton, { typeName: 'Ti.UI.Button' });
+        titaniumElementRegistry.registerElement('DashboardView', () => Ti.UI.createDashboardView, { typeName: 'Ti.UI.DashboardView' });
+        titaniumElementRegistry.registerElement('DashboardItem', () => Ti.UI.createDashboardItem, { typeName: 'Ti.UI.DashboardItem' });
+        titaniumElementRegistry.registerElement('ImageView', () => Ti.UI.createImageView, { typeName: 'Ti.UI.ImageView' });
+        titaniumElementRegistry.registerElement('Label', () => Ti.UI.createLabel, { typeName: 'Ti.UI.Label' });
         titaniumElementRegistry.registerElement('ListView', () => Ti.UI.createListView, { typeName: 'Ti.UI.ListView' });
+        titaniumElementRegistry.registerElement('ListItem', () => Ti.UI.createListItem, { skipAddToDom: true, typeName: 'Ti.UI.ListItem' });
         titaniumElementRegistry.registerElement('ListSection', () => Ti.UI.createListSection, { skipAddToDom: true, typeName: 'Ti.UI.ListSection' });
+        titaniumElementRegistry.registerElement('OptionDialog', () => Ti.UI.createOptionDialog, { typeName: 'Ti.UI.OptionDialog' });
+        titaniumElementRegistry.registerElement('Picker', () => Ti.UI.createPicker, { typeName: 'Ti.UI.Picker' });
+        titaniumElementRegistry.registerElement('PickerColumn', () => Ti.UI.createPickerColumn, { skipAddToDom: true, typeName: 'Ti.UI.PickerColumn' });
+        titaniumElementRegistry.registerElement('PickerRow', () => Ti.UI.createPickerRow, { skipAddToDom: true, typeName: 'Ti.UI.PickerRow' });
+        titaniumElementRegistry.registerElement('ProgressBar', () => Ti.UI.createProgressBar, { typeName: 'Ti.UI.ProgressBar' });
         titaniumElementRegistry.registerElement('RefreshControl', () => Ti.UI.createRefreshControl, { skipAddToDom: true, typeName: 'Ti.UI.RefreshControl' });
+        titaniumElementRegistry.registerElement('ScrollableView', () => Ti.UI.createScrollableView, { typeName: 'Ti.UI.ScrollableView' });
+        titaniumElementRegistry.registerElement('ScrollView', () => Ti.UI.createScrollView, { typeName: 'Ti.UI.ScrollView' });
+        titaniumElementRegistry.registerElement('SearchBar', () => Ti.UI.createSearchBar, { typeName: 'Ti.UI.SearchBar' });
+        titaniumElementRegistry.registerElement('Slider', () => Ti.UI.createSlider, { typeName: 'Ti.UI.Slider' });
+        titaniumElementRegistry.registerElement('Switch', () => Ti.UI.createSwitch, { typeName: 'Ti.UI.Switch' });
+        titaniumElementRegistry.registerElement('Tab', () => Ti.UI.createTab, { skipAddToDom: true, typeName: 'Ti.UI.Tab' });
         titaniumElementRegistry.registerElement('TabGroup', () => Ti.UI.createTabGroup, { skipAddToDom: true, typeName: 'Ti.UI.TabGroup' });
-        titaniumElementRegistry.registerElement('Tab', () => Ti.UI.createTab, {skipAddToDom: true, typeName: 'Ti.UI.Tab'});
+        titaniumElementRegistry.registerElement('TextArea', () => Ti.UI.createTextArea, { typeName: 'Ti.UI.TextArea' });
+        titaniumElementRegistry.registerElement('TextField', () => Ti.UI.createTextField, { typeName: 'Ti.UI.TextField' });
+        titaniumElementRegistry.registerElement('Toolbar', () => Ti.UI.createToolbar, { typeName: 'Ti.UI.Toolbar' });
+        titaniumElementRegistry.registerElement('TextField', () => Ti.UI.createTextField, { typeName: 'Ti.UI.TextField' });
+        titaniumElementRegistry.registerElement('View', () => Ti.UI.createView, { typeName: 'Ti.UI.View' });
+        titaniumElementRegistry.registerElement('WebView', () => Ti.UI.createWebView, { typeName: 'Ti.UI.WebView' });
+        titaniumElementRegistry.registerElement('Window', () => Ti.UI.createWindow, { skipAddToDom: true, typeName: 'Ti.UI.Window'});
     }
 
     onDestroy(callback: () => void): void {
-        this.platform.onDestroy(callback);
+        this._platform.onDestroy(callback);
     }
 
     get injector(): Injector {
-        return this.platform.injector;
+        return this._platform.injector;
     }
 
     destroy() {
@@ -93,6 +114,6 @@ export class TitaniumPlatformRef extends PlatformRef {
     }
 
     get destroyed() {
-        return this.platform.destroyed;
+        return this._platform.destroyed;
     }
 }
