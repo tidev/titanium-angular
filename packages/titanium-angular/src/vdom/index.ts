321c39461ef9d9d0fc81ff6f22c7731b0c1bb6a5
Jan Vennemann <jan.vennemann@gmx.net>
2017-12-15 01:47:16 +0100

    Remove non-existing export

--- packages/titanium-angular/src/vdom/index.ts
+++ packages/titanium-angular/src/vdom/index.ts
@@ -3,7 +3,6 @@ export * from './elements';
 export * from './ChildNodeList';
 export * from './CollectionIndexCache';
 export * from './ElementCollection';
-export * from './ElementFactory';
 export * from './NodeIterator';
 export * from './NodeListInterface';
 export * from './TitaniumElementRegistry';
