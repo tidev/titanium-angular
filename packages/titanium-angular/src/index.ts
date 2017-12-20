90d731b8c12cab71bd2bfbe8d5384bab7bb631cd
Jan Vennemann <jan.vennemann@gmx.net>
2017-12-20 08:36:03 +0100

    Remove unnecessary imports and exports

--- packages/titanium-angular/src/index.ts
+++ packages/titanium-angular/src/index.ts
@@ -4,6 +4,5 @@ export * from './log';
 export * from './platform';
 export * from './renderer';
 export * from './services';
-export * from './vdom';
 
 export { TitaniumModule } from './TitaniumModule';
