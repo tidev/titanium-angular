9ea26a2579e3cb8f8729eb26ae0b2597c05fa1e3
Jan Vennemann <jan.vennemann@gmx.net>
2017-12-20 18:42:13 +0100

    Rename default app component

--- ti-angular-example/app/src/app.module.ts
+++ ti-angular-example/app/src/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { TitaniumModule } from 'titanium-angular';
-import { RendererTestComponent } from './app.component';
+import { AppComponent } from './app.component';
 import { ItemComponent } from './item.component';
 import { TestComponent } from './test.component';
 
 @NgModule({
-    declarations: [RendererTestComponent, ItemComponent, TestComponent],
-    bootstrap: [RendererTestComponent],
+    declarations: [AppComponent, ItemComponent, TestComponent],
+    bootstrap: [AppComponent],
     imports: [TitaniumModule],
     schemas: [NO_ERRORS_SCHEMA]
 })
