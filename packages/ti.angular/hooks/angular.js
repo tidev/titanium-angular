eb9e5f57c13adfd48a28e7e6274edd72cad9ce9e
Jan Vennemann <jan.vennemann@gmx.net>
2018-03-04 22:48:25 -0300

    Remove unused files

--- packages/ti.angular/hooks/angular.js
+++
@@ -1,7 +0,0 @@
-const WebpackBundler = require('../lib/WebpackBundler');
-
-exports.id = 'ti.angular';
-exports.init = (logger, config, cli) => {
-	const bundler = new WebpackBundler(cli, logger);
-	bundler.initialize();
-};
